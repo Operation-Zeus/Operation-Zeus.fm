4b65f0f08a83c5223174a2d652722dd493c673b5
Luke Henke <luop90@gmx.com>
2016-10-10 20:40:30 -0600

    Removed http dependency

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+"use strict";
+
 /**
  * Include modules
  */
 const express = require('express');
-var app = express();
-const http = require('http').Server(app);
 const mysql = require('mysql');
 const fs = require('fs');
 const passport = require('passport');
@@ -11,21 +11,15 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const moment = require('moment');
 
-var api = require('./api.js');
-
-var User = require('./models/User.js');
+let app = express();
+let api = require('./api.js');
+let User = require('./models/User.js');
 
 // Setup our config file
-var config;
-if (fs.existsSync('./config/config.json')) {
-  config = JSON.parse(fs.readFileSync('./config/config.json'));
-} else {
-  console.error('Config file not found - Did you remember to rename config_example.json to config.json?');
-  process.exit(1);
-}
+let config = fs.readFileSync('config/config.json');
 
-var port = config.server.port;
-var domain = config.server.domain;
+let port = config.server.port;
+let domain = config.server.domain;
 
 // Set Jade as our view engine.
 app.set('views', `${__dirname}/views`);
@@ -40,6 +34,6 @@ app.use(passport.session());
 // import our routes
 require('./routes/routes.js')(app, passport);
 
-http.listen(port, () => {
+app.listen(port, () => {
   api.log('system', `Node server listening on ${port}`);
 });
