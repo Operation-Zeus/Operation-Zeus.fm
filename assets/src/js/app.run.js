16fdc49e40c4118c481e5223f05bde264bc3d7a7
Luke Henke <luop90@gmx.com>
2016-10-14 21:13:26 -0600

    Added TODO

--- assets/src/js/app.run.js
+++ assets/src/js/app.run.js
@@ -3,8 +3,8 @@ angular
   .run(runBlock);
 
 /* @ngInject */
-function runBlock($window, $rootScope, $location, $http, $state) {
-  $rootScope.fullyLoaded = true;
+function runBlock($window, $rootScope, $location, $http, $state, $cookies) {
+  // TODO: Show a welcome page if they've never been here before
 
-  
+  $rootScope.fullyLoaded = true;
 }
