532b0dfe56342433aeb999e67f2fd8f160d23b9a
Luke Henke <luop90@gmx.com>
2016-10-28 12:14:22 -0600

    Added route skeleton for backing up / deleting podcasts

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@
 
 let api = require('../api.js');
 let User = require('../models/User.js');
+let Podcast = require('../models/Podcast.js');
 
 module.exports = function (app, passport) {
   app.get('/', index);
@@ -9,6 +10,27 @@ module.exports = function (app, passport) {
 
   // HTML5 history
   app.get('*', index);
+
+  // API
+  app.post('/api/save/podcast/add', ensureAuthenticated, ensureValidBody, addPodcast);
+  app.post('/api/save/podcast/delete', ensureAuthenticated, ensureValidBody, deletePodcast);
+  app.post('/api/save/episode/update-time', ensureAuthenticated, ensureValidBody, saveEpisodeTime);
+};
+
+var ensureAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.sendStatus(401);
+};
+
+var ensureValidBody = function (req, res, next) {
+  if (!req.body || !req.body.podcast) {
+    return res.sendStatus(400);
+  }
+
+  next();
 };
 
 var index = function (req, res) {
@@ -19,4 +41,40 @@ var index = function (req, res) {
 
 var partials = function (req, res) {
   res.render(`partials/${req.params.name}`);
-}
+};
+
+var addPodcast = function (req, res) {
+  let response = {};
+
+  Podcast.addPodcast({
+    user: req.user,
+    podcast: req.body.podcast
+  }, (error) => {
+    if (error) {
+      response.success = false;
+      response.error = error;
+      return res.json(response);
+    }
+
+    response.success = true;
+    res.json(response);
+  });
+};
+
+var deletePodcast = function (req, res) {
+  let response = {};
+
+  Podcast.deletePodcast({
+    user: req.user,
+    podcast: req.body.podcast
+  }, (error) => {
+    if (error) {
+      response.success = false;
+      response.error = error;
+      return res.json(response);
+    }
+
+    response.success = true;
+    res.json(response);
+  });
+};
