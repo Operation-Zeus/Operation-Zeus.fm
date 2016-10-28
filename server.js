d177d6b3b6fa00b23339bcf10877598bdf78fa12
Luke Henke <luop90@gmx.com>
2016-10-28 12:59:41 -0600

    Added passport skeleton for auth

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express');
 const mysql = require('mysql');
 const fs = require('fs');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const moment = require('moment');
@@ -20,6 +21,36 @@ let config = JSON.parse(fs.readFileSync('config/config.json'));
 
 let port = config.server.port;
 let domain = config.server.domain;
+
+/* Passport setup */
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password'
+},
+  function (username, password, done) {
+
+}));
+
+// Setup our session
+app.use(session({
+  secret: config.server.cookieSecret, // The secret is like the "salt" to our cookie. Makes sure that others cannot fake it.
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7 * 52
+  },
+  resave: true,
+  saveUninitialized: true,
+  key: 'zeus.sid'
+}));
 
 // Set Jade as our view engine.
 app.set('views', `${__dirname}/views`);
