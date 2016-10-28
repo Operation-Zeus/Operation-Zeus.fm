c6dc6fede4202bc816c95c580fee74f26e7513db
Luke Henke <luop90@gmx.com>
2016-10-28 12:18:56 -0600

    Added skeleton to podcast model

---
+++ models/Podcast.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let api = require('../api.js');
+
+let Podcast = {};
+
+/**
+ * Adds a podcast to the user's list
+ * @param {OBJECT} data
+ * @param {FUNCTION} callback
+ */
+Podcast.addPodcast = function (data, callback) {
+
+};
+
+/**
+ * Deltes a podcast from the user's list
+ * @param {OBJECT} data
+ * @param {FUNCTION} callback
+ */
+Podcast.deletePodcast = function (data, callback) {
+
+};
+
+module.exports = Podcast;
