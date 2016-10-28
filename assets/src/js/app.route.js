22576623c13b9f3fb28c65e216030bda5d426022
Luke Henke <luop90@gmx.com>
2016-10-28 14:25:52 -0600

    Added controllers to routes

--- assets/src/js/app.route.js
+++ assets/src/js/app.route.js
@@ -3,15 +3,17 @@ function routeConfig($stateProvider, $locationProvider) {
   $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: '/partials/home'
+      templateUrl: '/partials/home',
+      controller: HomePageCtrl
     })
     .state('welcome', {
       url: '/welcome',
-      templateUrl: '/partials/welcome'
+      templateUrl: '/partials/welcome',
+      controller: WelcomePageCtrl
     })
     .state('login', {
       templateUrl: '/partials/login'
     });
-    
+
   $locationProvider.html5Mode(true);
 }
